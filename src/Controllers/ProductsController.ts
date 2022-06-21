5a6152ffb349e602cfa4af6c3f3a384f9a9ba062
luisa-laf <106617801+luisa-laf@users.noreply.github.com>
2022-06-21 14:26:49 -0300

    Update ProductsController.ts

--- src/Controllers/ProductsController.ts
+++ src/Controllers/ProductsController.ts
@@ -1,35 +1,11 @@
-async update(req: Request, res: Response) {
+async vinculateCategory(req: Request, res: Response) {
 const { id } = req.params;
-const { name, value, image, description, quantities, categories } =
-req.body;
+const { categories } = req.body;
 const { is_admin: isAdmin } = req;
 if (isAdmin) {
-if (
-!name &&
-!value &&
-!image &&
-!description &&
-!quantities &&
-!categories
-) {
-return res.status(400).json({
-error: {
-message: `Some field is required: name, value, image, quantities, categories`,
-},
-});
-}
-const service = new UpdateProductService();
+const service = new LinkProductToCategory();
 try {
-const result = await service.execute(
-id,
-name,
-value,
-image,
-description
-,
-quantities,
-categories
-);
+const result = await service.execute(id, categories);
 return res.json(result);
 } catch (err)
 { return res
@@ -40,18 +16,19 @@ return res.json(result);
 return
 res
 .status(401)
-.json({ error: "Only admins can update products." });
+.json({ error: "Only admins can vinculate products." });
 }
-async delete(req: Request, res: Response) {
+async vinculateSize(req: Request, res: Response) {
 const { id } = req.params;
+const { quantities } = req.body;
 const { is_admin: isAdmin } = req;
 if (isAdmin) {
-const service = new DeleteProductService();
+const service = new LinkProductToSize();
 try {
-const result = await service.execute(String(id));
+const result = await service.execute(id, quantities);
 return res.json(result);
-} catch (err) {
-return res
+} catch (err)
+{ return res
 .status(err.code ?? 400)
 .json({ error: err.error ?? err.message });
 }
@@ -59,5 +36,6 @@ return res
 return
 res
 .status(401)
-.json({ error: "Only admins can delete products." });
+.json({ error: "Only admins can vinculate products." });
 }
+

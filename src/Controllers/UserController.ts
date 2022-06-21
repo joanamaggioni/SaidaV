415482c1de3a26d8b78276c878294b7b1dd8b64a
luisa-laf <106617801+luisa-laf@users.noreply.github.com>
2022-06-21 14:38:32 -0300

    Update UserController.ts

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -1,14 +1,11 @@
-import { Request, Response } from "express";
-import { AuthenticateUserService } from "../services/User/AuthenticateUserService";
-import { CreateUserService } from "../services/User/CreateUserService";
-import { UpdateUserService } from "../services/User/UpdateUserService";
-import { GetAllUsersService } from "../services/User/GetAllUsersService";
-class UserController {
-async authenticate(req: Request, res: Response) {
-const { email, password } = req.body;
+async create(req: Request, res: Response) {
+const { email, password, name, adress, phone, last_name } = req.body;
 const errors: String[] = [];
 !email && errors.push("email");
 !password && errors.push("password");
+!name && errors.push("name");
+!adress && errors.push("adress");
+!phone && errors.push("phone");
 if (errors.length !== 0) {
 return
 res.status(400).json({ error:
@@ -20,9 +17,16 @@ errors.length === 1
 },
 });
 }
-const service = new AuthenticateUserService();
+const service = new CreateUserService();
 try {
-const result = await service.execute(email, password);
+const result = await service.execute(
+email,
+password
+, name,
+adress,
+phone,
+last_name
+);
 return res.json(result);
 } catch (err)
 { return res
@@ -30,4 +34,13 @@ return res.json(result);
 .json({ error: err.error ?? err.message });
 }
 }
-
+async update(req: Request, res: Response) {
+const { user_id } = req;
+const { email, password, name, adress, phone, last_name } = req.body;
+if (!email && !password && !name && !last_name && !adress && !phone) {
+return res.status(400).json({
+error: {
+message: `Some field is required: email, password, name, adress, phone, last_name`,
+},
+});
+}

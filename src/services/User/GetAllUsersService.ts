3e510494028c9afb09b8e18fd13382a4f92729df
Joana Maggioni <100417975+joanamaggioni@users.noreply.github.com>
2022-06-21 14:18:03 -0300

    Update GetAllUsersService.ts

--- src/services/User/GetAllUsersService.ts
+++ src/services/User/GetAllUsersService.ts
@@ -1,1 +1,17 @@
+import prismaClient from "../../prisma";
+import bcrypt from "bcrypt";
+
+class GetAllUsersService {
+  async execute() {
+    const users = await prismaClient.user.findMany();
+
+    users.map((item) => {
+      delete item.password;
+    });
+
+    return users;
+  }
+}
+
+export { GetAllUsersService };
 

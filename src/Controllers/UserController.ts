7f312e49afce9fca90b4d6e20850154165601003
luisa-laf <106617801+luisa-laf@users.noreply.github.com>
2022-06-21 14:39:04 -0300

    Update UserController.ts

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -1,28 +1,10 @@
-async create(req: Request, res: Response) {
-const { email, password, name, adress, phone, last_name } = req.body;
-const errors: String[] = [];
-!email && errors.push("email");
-!password && errors.push("password");
-!name && errors.push("name");
-!adress && errors.push("adress");
-!phone && errors.push("phone");
-if (errors.length !== 0) {
-return
-res.status(400).json({ error:
-{
-message:
-errors.length === 1
-? `Field is required: ${errors[0]}`
-: `Fields are required: ${errors.join(", ")}`,
-},
-});
-}
-const service = new CreateUserService();
+const service = new UpdateUserService();
 try {
 const result = await service.execute(
+user_id,
 email,
-password
-, name,
+password,
+name,
 adress,
 phone,
 last_name
@@ -34,13 +16,20 @@ return res.json(result);
 .json({ error: err.error ?? err.message });
 }
 }
-async update(req: Request, res: Response) {
-const { user_id } = req;
-const { email, password, name, adress, phone, last_name } = req.body;
-if (!email && !password && !name && !last_name && !adress && !phone) {
-return res.status(400).json({
-error: {
-message: `Some field is required: email, password, name, adress, phone, last_name`,
-},
-});
+async listAll(req: Request, res: Response) {
+const { is_admin: isAdmin } = req;
+const service = new GetAllUsersService();
+if (isAdmin) {
+try {
+const result = await service.execute();
+return res.json(result);
+} catch (err)
+{ return res
+.status(err.code ?? 400)
+.json({ error: err.error ?? err.message });
 }
+} else
+return res.status(401).json({ error: "Only admins can create sizes." });
+}
+}
+export { UserController };

0e80faa361e9c564f0f850ccbe65e267d3f48a46
luisa-laf <106617801+luisa-laf@users.noreply.github.com>
2022-06-21 14:37:04 -0300

    Update SizesController.ts

--- src/Controllers/SizesController.ts
+++ src/Controllers/SizesController.ts
@@ -1,1 +1,29 @@
-
+import { Request, Response } from "express";
+import { GetAllSizesServices } from "../services/Products/Sizes/GetAllSizesServices";
+import { DeleteSizeService } from "../services/Products/Sizes/DeleteSizeService";
+import { CreateSizesService } from "../services/Products/Sizes/CreateSizesService";
+class SizesController {
+async create(req: Request, res: Response) {
+const { name } = req.body;
+const { is_admin: isAdmin } = req;
+if (isAdmin) {
+const errors: String[] = [];
+!name && errors.push("name");
+if (errors.length !== 0) {
+return
+res.status(400).json({
+error: `Field is required: ${errors[0]}`,
+});
+}
+const service = new CreateSizesService();
+try {
+const result = await service.execute(name);
+return res.json(result);
+} catch (err)
+{ return res
+.status(err.code ?? 400)
+.json({ error: err.error ?? err.message });
+}
+} else
+return res.status(401).json({ error: "Only admins can create sizes." });
+}
